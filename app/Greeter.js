ae6aec8ba32a17a38160f8b3c16b54fbe9e11896
xinxin <xinxin@rrkd.cn>
2016-11-01 18:17:05 +0800

    add files

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,16 @@
-module.exports = function(){
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
+//Greeter,js
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './Greeter.css';
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}>
+        {config.greetText}
+      </div>
+    );
+  }
 }
 
-
+export default Greeter
